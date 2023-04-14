b17a730dc7034e3cab2fec692fa6203b567d76b1
Rafael Klaus Pilz <kp.rafael@gmail.com>
2023-04-14 18:20:19 -0300

    adicionando menu interativo

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.Scanner;
+
 import arvore.ArvoreBinaria;
 import ui.Arvore;
 
@@ -7,14 +9,25 @@ public class Main {
 
 	public static void main(String[] args) {
 		ArvoreBinaria arvoreBinaria = new ArvoreBinaria();
+		
+		Scanner scanner = new Scanner(System.in);
+		boolean flag = true;
+		int numero = 0;
+		while(flag) {
+			System.out.println("Digite a opcao que deseja: ");
+            numero = scanner.nextInt();
+            arvoreBinaria.inserir(numero);
+            Arvore.printNode(arvoreBinaria.getRaiz());
+		}
+		
 
-		arvoreBinaria.inserir(4);
-		arvoreBinaria.inserir(7);
-		arvoreBinaria.inserir(6);
-		arvoreBinaria.inserir(4);
-		arvoreBinaria.inserir(2);
+		//arvoreBinaria.inserir(4);
+		//arvoreBinaria.inserir(7);
+		//arvoreBinaria.inserir(6);
+		//arvoreBinaria.inserir(4);
+		//arvoreBinaria.inserir(2);
 
-		Arvore.printNode(arvoreBinaria.getRaiz());
+		//Arvore.printNode(arvoreBinaria.getRaiz());
 	}
 }
 
